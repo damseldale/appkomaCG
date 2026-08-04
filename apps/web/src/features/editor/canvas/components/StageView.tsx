006a4a0c96a78cbc385fb51649c3aad9d39f292f
damseldale <salmandzaky126@gmail.com>
2026-08-05 04:54:46 +0700

    Update StageView.tsx

--- apps/web/src/features/editor/canvas/components/StageView.tsx
+++ apps/web/src/features/editor/canvas/components/StageView.tsx
@@ -1,35 +1,22 @@
 "use client";
 
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage } from "react-konva";
 
-import {
-  CANVAS_HEIGHT,
-  CANVAS_WIDTH,
-} from "../constants";
+import { World } from "./World";
 
 import { useCanvasStore } from "../store";
 
 export function StageView() {
-  const camera = useCanvasStore(
-    (state) => state.camera,
+  const viewport = useCanvasStore(
+    (state) => state.viewport,
   );
 
   return (
     <Stage
-      width={CANVAS_WIDTH}
-      height={CANVAS_HEIGHT}
-      scaleX={camera.zoom}
-      scaleY={camera.zoom}
-      x={camera.x}
-      y={camera.y}
+      width={viewport.width}
+      height={viewport.height}
     >
-      <Layer>
-        <Rect
-          width={CANVAS_WIDTH}
-          height={CANVAS_HEIGHT}
-          fill="white"
-        />
-      </Layer>
+      <World />
     </Stage>
   );
 }
