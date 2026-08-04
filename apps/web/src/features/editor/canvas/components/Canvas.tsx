92cf2d448a7192d2bc4804be5b505a603c3ba22e
damseldale <salmandzaky126@gmail.com>
2026-08-05 04:55:26 +0700

    Update Canvas.tsx

--- apps/web/src/features/editor/canvas/components/Canvas.tsx
+++ apps/web/src/features/editor/canvas/components/Canvas.tsx
@@ -1,30 +1,14 @@
 "use client";
 
-import { CanvasContainer }
-from "./CanvasContainer";
-
-import { StageView }
-from "./StageView";
+import { CanvasContainer } from "./CanvasContainer";
+import { StageView } from "./StageView";
 
 export function Canvas() {
-
-    return (
-
-        <section
-        className="
-        flex
-        flex-1
-        bg-neutral-800
-        ">
-
-            <CanvasContainer>
-
-                <StageView/>
-
-            </CanvasContainer>
-
-        </section>
-
-    );
-
+  return (
+    <section className="flex flex-1 overflow-hidden bg-neutral-800">
+      <CanvasContainer>
+        <StageView />
+      </CanvasContainer>
+    </section>
+  );
 }
