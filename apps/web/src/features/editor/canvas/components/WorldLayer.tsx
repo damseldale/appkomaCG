f06218b10fce1df954570aaa6919acff8750ecd3
damseldale <salmandzaky126@gmail.com>
2026-08-05 06:47:34 +0700

    Update WorldLayer.tsx

--- apps/web/src/features/editor/canvas/components/WorldLayer.tsx
+++ apps/web/src/features/editor/canvas/components/WorldLayer.tsx
@@ -1,41 +1,35 @@
 "use client";
 
-import type { PropsWithChildren } from "react";
-import { Group, Layer, Rect } from "react-konva";
-
-import {
-  CANVAS_HEIGHT,
-  CANVAS_WIDTH,
-} from "../constants";
+import { PropsWithChildren } from "react";
+import { Group, Rect } from "react-konva";
 
 import { useCanvasStore } from "../store";
 
-interface WorldLayerProps
-  extends PropsWithChildren {}
-
 export function WorldLayer({
   children,
-}: WorldLayerProps) {
+}: PropsWithChildren) {
   const camera = useCanvasStore(
     (state) => state.camera,
   );
 
   return (
-    <Layer>
-      <Group
-        x={camera.x}
-        y={camera.y}
-        scaleX={camera.zoom}
-        scaleY={camera.zoom}
-      >
-        <Rect
-          width={CANVAS_WIDTH}
-          height={CANVAS_HEIGHT}
-          fill="white"
-        />
+    <Group
+      x={camera.x}
+      y={camera.y}
+      scaleX={camera.zoom}
+      scaleY={camera.zoom}
+    >
+      {/* Canvas / Scene Background */}
+      <Rect
+        x={0}
+        y={0}
+        width={1920}
+        height={1080}
+        fill="#ffffff"
+        listening={false}
+      />
 
-        {children}
-      </Group>
-    </Layer>
+      {children}
+    </Group>
   );
 }
