5a3aded74f25e42b1cea3bb89a68b694cf72e80c
damseldale <salmandzaky126@gmail.com>
2026-08-05 04:46:23 +0700

    Update Canvas.tsx

--- apps/web/src/features/editor/canvas/components/Canvas.tsx
+++ apps/web/src/features/editor/canvas/components/Canvas.tsx
@@ -1,13 +1,30 @@
 "use client";
 
-import { StageView } from "./StageView";
+import { CanvasContainer }
+from "./CanvasContainer";
+
+import { StageView }
+from "./StageView";
 
 export function Canvas() {
-  return (
-    <section className="flex flex-1 items-center justify-center overflow-auto bg-neutral-800">
-      <div className="rounded-xl shadow-2xl">
-        <StageView />
-      </div>
-    </section>
-  );
+
+    return (
+
+        <section
+        className="
+        flex
+        flex-1
+        bg-neutral-800
+        ">
+
+            <CanvasContainer>
+
+                <StageView/>
+
+            </CanvasContainer>
+
+        </section>
+
+    );
+
 }
