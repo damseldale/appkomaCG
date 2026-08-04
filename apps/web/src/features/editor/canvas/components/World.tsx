8b8bddafe8ed45988ba59e9358bee2284df5af30
damseldale <salmandzaky126@gmail.com>
2026-08-05 04:54:11 +0700

    Update World.tsx

--- apps/web/src/features/editor/canvas/components/World.tsx
+++ apps/web/src/features/editor/canvas/components/World.tsx
@@ -1,22 +1,41 @@
 "use client";
 
-import { Layer, Rect } from "react-konva";
+import { Group, Layer, Rect } from "react-konva";
 
 import {
   CANVAS_HEIGHT,
   CANVAS_WIDTH,
 } from "../constants";
 
-export function World() {
+import { useCanvasStore } from "../store";
+
+interface WorldProps {
+  children?: React.ReactNode;
+}
+
+export function World({
+  children,
+}: WorldProps) {
+  const camera = useCanvasStore(
+    (state) => state.camera,
+  );
+
   return (
     <Layer>
-      <Rect
-        x={0}
-        y={0}
-        width={CANVAS_WIDTH}
-        height={CANVAS_HEIGHT}
-        fill="white"
-      />
+      <Group
+        x={camera.x}
+        y={camera.y}
+        scaleX={camera.zoom}
+        scaleY={camera.zoom}
+      >
+        <Rect
+          width={CANVAS_WIDTH}
+          height={CANVAS_HEIGHT}
+          fill="white"
+        />
+
+        {children}
+      </Group>
     </Layer>
   );
 }
