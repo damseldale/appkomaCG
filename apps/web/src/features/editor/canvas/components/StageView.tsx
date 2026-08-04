1dd4e2b50327afb32852bdeb695cf85a43bcb94b
damseldale <salmandzaky126@gmail.com>
2026-08-05 04:53:12 +0700

    Update StageView.tsx

--- apps/web/src/features/editor/canvas/components/StageView.tsx
+++ apps/web/src/features/editor/canvas/components/StageView.tsx
@@ -7,16 +7,24 @@ import {
   CANVAS_WIDTH,
 } from "../constants";
 
+import { useCanvasStore } from "../store";
+
 export function StageView() {
+  const camera = useCanvasStore(
+    (state) => state.camera,
+  );
+
   return (
     <Stage
       width={CANVAS_WIDTH}
       height={CANVAS_HEIGHT}
+      scaleX={camera.zoom}
+      scaleY={camera.zoom}
+      x={camera.x}
+      y={camera.y}
     >
       <Layer>
         <Rect
-          x={0}
-          y={0}
           width={CANVAS_WIDTH}
           height={CANVAS_HEIGHT}
           fill="white"
