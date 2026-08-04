9cd0f7a38ebe139241f48bd777f87eb209452701
damseldale <salmandzaky126@gmail.com>
2026-08-05 04:33:02 +0700

    Create constants.ts

---
+++ apps/web/src/features/editor/canvas/constants.ts
@@ -0,0 +1,9 @@
+export const CANVAS_WIDTH = 1280;
+export const CANVAS_HEIGHT = 720;
+
+export const GRID_SIZE = 32;
+
+export const MIN_ZOOM = 0.2;
+export const MAX_ZOOM = 4;
+
+export const DEFAULT_ZOOM = 1;
