ea0e25173efa7021c4ec067ae1069b4662364015
damseldale <salmandzaky126@gmail.com>
2026-08-05 04:47:40 +0700

    Create canvasStore.ts

---
+++ apps/web/src/features/editor/canvas/store/canvasStore.ts
@@ -0,0 +1,56 @@
+"use client";
+
+import { create } from "zustand";
+
+import type {
+  CameraState,
+  CanvasState,
+  ViewportState,
+} from "./types";
+
+interface CanvasActions {
+  setViewport(
+    viewport: ViewportState,
+  ): void;
+
+  setCamera(
+    camera: Partial<CameraState>,
+  ): void;
+
+  resetCamera(): void;
+}
+
+const initialCamera: CameraState = {
+  x: 0,
+  y: 0,
+  zoom: 1,
+};
+
+export const useCanvasStore = create<
+  CanvasState & CanvasActions
+>((set) => ({
+  viewport: {
+    width: 0,
+    height: 0,
+  },
+
+  camera: initialCamera,
+
+  setViewport: (viewport) =>
+    set({
+      viewport,
+    }),
+
+  setCamera: (camera) =>
+    set((state) => ({
+      camera: {
+        ...state.camera,
+        ...camera,
+      },
+    })),
+
+  resetCamera: () =>
+    set({
+      camera: initialCamera,
+    }),
+}));
