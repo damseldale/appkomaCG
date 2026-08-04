32536bf904d61442dab70855e52e10b5d73fcdff
damseldale <salmandzaky126@gmail.com>
2026-08-05 06:50:07 +0700

    Create GroupRenderer.tsx

---
+++ apps/web/src/features/scene/render/renderers/GroupRenderer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import type { GroupObject } from "@/features/objects";
+
+interface Props {
+  object: GroupObject;
+}
+
+export function GroupRenderer({
+  object,
+}: Props) {
+  void object;
+
+  return null;
+}
