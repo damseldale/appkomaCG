890c5ac17ad4c18a9d6504921a0f0c6fc480a816
damseldale <salmandzaky126@gmail.com>
2026-08-05 11:35:53 +0700

    Update CharacterRenderer.tsx

--- apps/web/src/features/scene/render/renderers/CharacterRenderer.tsx
+++ apps/web/src/features/scene/render/renderers/CharacterRenderer.tsx
@@ -1,34 +1,62 @@
 "use client";
 
-import { Circle, Text } from "react-konva";
+import { Group, Circle, Text } from "react-konva";
 
 import type { CharacterObject } from "@/features/objects";
+import { useSceneStore } from "../../store";
 
 interface Props {
-  object: CharacterObject;
+    object: CharacterObject;
 }
 
 export function CharacterRenderer({
-  object,
+    object,
 }: Props) {
-  return (
-    <>
-      <Circle
-        x={object.transform.x}
-        y={object.transform.y}
-        radius={32}
-        fill="#8ec5ff"
-        stroke="#2563eb"
-        strokeWidth={2}
-      />
 
-      <Text
-        x={object.transform.x - 40}
-        y={object.transform.y + 42}
-        width={80}
-        align="center"
-        text={object.name}
-      />
-    </>
-  );
+    const selectedIds = useSceneStore(
+        state => state.selectedIds,
+    );
+
+    const selectObject = useSceneStore(
+        state => state.selectObject,
+    );
+
+    const selected = selectedIds.includes(
+        object.id,
+    );
+
+    return (
+        <Group
+            x={object.transform.x}
+            y={object.transform.y}
+            onClick={(e) => {
+                e.cancelBubble = true;
+                selectObject(object.id);
+            }}
+        >
+            <Circle
+                radius={40}
+                fill="#FACC15"
+                stroke={
+                    selected
+                        ? "#2563EB"
+                        : "#EAB308"
+                }
+                strokeWidth={
+                    selected
+                        ? 3
+                        : 2
+                }
+            />
+
+            <Text
+                y={52}
+                width={80}
+                x={-40}
+                align="center"
+                text="Character"
+                listening={false}
+            />
+        </Group>
+    );
 }
