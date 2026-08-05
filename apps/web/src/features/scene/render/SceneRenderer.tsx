ec5bfaa15bef88b730daed299b8fd34fd3d1d5f9
damseldale <salmandzaky126@gmail.com>
2026-08-05 11:25:02 +0700

    Update SceneRenderer.tsx

--- apps/web/src/features/scene/render/SceneRenderer.tsx
+++ apps/web/src/features/scene/render/SceneRenderer.tsx
@@ -2,12 +2,26 @@
 
 import { useEffect } from "react";
 
+import { useSceneObjects } from "../selectors";
+
 import { bootstrapRenderers } from "./bootstrap";
+import { ObjectRenderer } from "./ObjectRenderer";
 
 export function SceneRenderer() {
+    const objects = useSceneObjects();
+
     useEffect(() => {
         bootstrapRenderers();
     }, []);
 
-    ...
+    return (
+        <>
+            {objects.map((object) => (
+                <ObjectRenderer
+                    key={object.id}
+                    object={object}
+                />
+            ))}
+        </>
+    );
 }
