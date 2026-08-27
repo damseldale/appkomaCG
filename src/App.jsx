cb68f9f4ec983df35c478811c7716d9d51621ed5
damseldale <salmandzaky126@gmail.com>
2026-08-27 17:42:42 +0700

    feat(assets): mount asset library in editor workspace

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import CanvasEditor from './components/CanvasEditor';
 import Timeline from './components/Timeline';
 import Inspector from './components/Inspector';
 import Navbar from './components/Navbar';
+import AssetLibrary from './components/AssetLibrary';
 import './App.css';
 import { useSceneHistoryShortcuts } from './features/scene/history/useSceneHistory';
 
 function App() {
   useSceneHistoryShortcuts();
 
-  useEffect(() => {
-    return () => {};
-  }, []);
+  useEffect(() => () => {}, []);
 
   return (
     <div className="app-layout">
       <Navbar />
       <div className="workspace">
+        <AssetLibrary />
         <CanvasEditor />
         <Inspector />
       </div>
