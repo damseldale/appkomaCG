a224771c99d43fcabfc8f01052df82d508693895
damseldale <salmandzaky126@gmail.com>
2026-08-27 17:58:15 +0700

    feat(editor): integrate phase 10 toolbar and project controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import { useEffect } from 'react';
 import CanvasEditor from './components/CanvasEditor';
 import Timeline from './components/Timeline';
 import Inspector from './components/Inspector';
 import Navbar from './components/Navbar';
 import AssetLibrary from './components/AssetLibrary';
+import EditorToolbar from './components/EditorToolbar';
+import ProjectPersistencePanel from './components/ProjectPersistencePanel';
 import './App.css';
-import { useSceneHistoryShortcuts } from './features/scene/history/useSceneHistory';
+import './components/EditorToolbar.css';
 
 function App() {
-  useSceneHistoryShortcuts();
-
-  useEffect(() => () => {}, []);
-
   return (
     <div className="app-layout">
       <Navbar />
+      <EditorToolbar />
       <div className="workspace">
         <AssetLibrary />
         <CanvasEditor />
         <Inspector />
       </div>
       <Timeline />
+      <ProjectPersistencePanel />
     </div>
   );
 }
