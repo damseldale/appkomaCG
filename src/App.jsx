94d3be1efbf8cf268f965d9c01afcd0f0cbb1ee5
damseldale <salmandzaky126@gmail.com>
2026-08-27 17:31:09 +0700

    feat(history): enable global undo redo shortcuts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import CanvasEditor from './components/CanvasEditor'
-import Timeline from './components/Timeline'
-import Inspector from './components/Inspector'
-import Navbar from './components/Navbar'
-import './App.css'
+import { useEffect } from 'react';
+import CanvasEditor from './components/CanvasEditor';
+import Timeline from './components/Timeline';
+import Inspector from './components/Inspector';
+import Navbar from './components/Navbar';
+import './App.css';
+import { useSceneHistoryShortcuts } from './features/scene/history/useSceneHistory';
 
 function App() {
+  useSceneHistoryShortcuts();
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
   return (
     <div className="app-layout">
       <Navbar />
@@ -14,7 +22,7 @@ function App() {
       </div>
       <Timeline />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
